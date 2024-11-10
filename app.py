f0d40ee3a8b0192bafec9edc41f5989c13df69a2
Benjamin Workman <bdworkma@gmail.com>
2024-11-09 21:29:16 -0500

    Preprocessing added

--- app.py
+++ app.py
@@ -11,24 +11,35 @@ st.write("Raw DataFrame:", df_vehicles)
 
 
 
-fig = px.histogram(
-    df_vehicles,
-    x = 'days_listed',
-    color = 'type'
-)
+# Check for Duplicated Values and null values
+vehicle_duplicates = df_vehicles[df_vehicles.duplicated]
 
-fig.show()
+#Truncate the values for the model column to include just the maker of the car.  
+# This provides a simplifed view; all models for each maker would make an extremely large amount of values on the respective axis
+df_vehicles['model'] = df_vehicles['model'].apply(lambda x: x.split()[0])
+print(df_vehicles['model'])
+
+#Empty dataframe is returned, therefore there are no duplicates
+
+#In the model_year column, we have null values.  Without these values, we cannot accurately assess a cars values, as there are notable differences between years 
+#These empty columns will be changed the median per model
+#Use transform function to add the median model year for each model to null values
+model_median = df_vehicles.groupby('model')['model_year'].median()
+df_vehicles['model_year'] = df_vehicles.groupby('model')['model_year'].transform(lambda x: x.fillna(model_median[x.name]))
+
+odometer_mean = df_vehicles.groupby('model')['odometer'].mean()
+df_vehicles['odometer'] = df_vehicles.groupby('model')['odometer'].transform(lambda x: x.fillna(odometer_mean[x.name]))
+
+cylinders_median = df_vehicles.groupby('model')['cylinders'].median()
+df_vehicles['cylinders'] = df_vehicles.groupby('model')['cylinders'].transform(lambda x: x.fillna(cylinders_median[x.name]))
 
 
-"""
-Next, we create a scatter plot for three variables in plotly: 
-(1.) Price (2.) Odometer Reading (3.) Model_Year
-Price is lowered to exclude outliers and make the graph more readable
-As we can see from the graph, a newer year and lower odometer reading,
-as one would suspect, yields a higher price typically
-"""
-df_vehicles['price'] = df_vehicles['price'][df_vehicles['price'] < 30000]
-df_vehicles['odometer'] = df_vehicles['odometer'].dropna()
+#Check to see if the null values have been filled
+df2 = df_vehicles['model_year'][df_vehicles['model_year'].isna()]
+df3 = df_vehicles['odometer'][df_vehicles['odometer'].isna()]
+df4 = df_vehicles['cylinders'][df_vehicles['cylinders'].isna()]
+
+
 
 show_hist = st.checkbox("Show Histogram")
 show_scatterplot = st.checkbox("Show Scatterplot")
